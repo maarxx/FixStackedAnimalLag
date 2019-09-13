fa0af416e3234d3c9d5c389050f0df3e28fddfa6
Chris Lewark <chris.lewark@gmail.com>
2019-09-13 05:53:51 -0400

    Initial Commit

---
+++ FixStackedAnimalLag/Patches.cs
@@ -0,0 +1,40 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace FixStackedAnimalLag
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.fixstackedanimallag");
+            var harmony = HarmonyInstance.Create("com.github.harmony.rimworld.maarx.fixstackedanimallag");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    //public static Vector3 PawnCollisionPosOffsetFor(Pawn pawn)
+    [HarmonyPatch(typeof(PawnCollisionTweenerUtility))]
+    [HarmonyPatch("PawnCollisionPosOffsetFor")]
+    class PawnCollisionTweenerUtility_PawnCollisionPosOffsetFor
+    {
+        static bool Prefix(ref Pawn pawn, ref Vector3 __result)
+        {
+            if (pawn != null && pawn.AnimalOrWildMan())
+            {
+                __result = Vector3.zero;
+                return false;
+            }
+            return true;
+            //Log.Message("Hello from Harmony PawnCollisionTweenerUtility_PawnCollisionPosOffsetFor Prefix");
+        }
+    }
+}
